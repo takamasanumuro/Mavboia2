e4449be59b79d3eb943560b6b21ffa37f1b60fba
adrianowerneck <adrianowerneck@id.uff.br>
2025-03-03 22:44:33 -0300

    Add GMap.NET support and enhance MapView UI
    Updated `Core.csproj` to include GMap.NET packages for mapping.
    Modified `MainWindow.xaml` to center and maximize the window.
    Enhanced `MapView.xaml` with a `GMapControl` and additional UI elements for displaying boat metrics.
    Updated `MapView.xaml.cs` to configure the map, add a boat marker, and ensure proper disposal to prevent memory leaks.

--- Core/Views/MapView.xaml.cs
+++ Core/Views/MapView.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using Core.ViewModels;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
 using ReactiveUI;
 using System;
 using System.Collections.Generic;
@@ -20,9 +23,45 @@ namespace Core.Views
     public class MapViewBase : ReactiveUserControl<MapViewModel> { }
     public partial class MapView : MapViewBase
     {
+        private GMapMarker _boatMarker;
         public MapView()
         {
             InitializeComponent();
+
+            Map.MapProvider = GMap.NET.MapProviders.GoogleHybridMapProvider.Instance;
+            Map.Position = new GMap.NET.PointLatLng(-22.8833, -43.1034);
+            Map.MinZoom = 2;
+            Map.MaxZoom = 17;
+            Map.Zoom = 9;
+            Map.DragButton = MouseButton.Left;
+            Map.Manager.Mode = GMap.NET.AccessMode.ServerAndCache;
+
+            AddBoatMarker(new PointLatLng(-22.8833, -43.1034));
+
         }
+        ~MapView()
+        {
+            Map.Dispose(); //Necessary to prevent memory leaks that can cause the process to remain in memory after closing the application
+        }
+
+        private void AddBoatMarker(PointLatLng position)
+        {
+            _boatMarker = new GMapMarker(position)
+            {
+                Shape = new Ellipse
+                {
+                    Width = 10,
+                    Height = 10,
+                    Stroke = Brushes.OrangeRed,
+                    StrokeThickness = 1.5
+                },
+                Tag = "Boat"
+            };
+
+            Map.Markers.Add(_boatMarker);
+            
+        }
+
+
     }
 }
